d990d725bcbe6836a80d072e270a479421b43cb0
SKShehan <shehansilva2013@gmail.com>
2022-05-23 18:52:24 +0530

    Student and Group Re

--- App.jsx
+++ App.jsx
@@ -1,13 +1,15 @@
 import  { React} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Registration from "./components/supervisor/registration";
+import RegisterStudent from "./components/Views/Student/Registration";
 
 export default function App() {
   return(
     <div>
       <BrowserRouter>
       <Routes>
-      <Route path="/register" element = {<Registration/>} exact/>
+      <Route path="/register" element = {<Registration/>} />
+      <Route path="/register-student" element = {<RegisterStudent/>} />
       </Routes>
       </BrowserRouter>
     </div>
